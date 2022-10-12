c357fa275ef30d51c04c424778b3d1a79d4c2134
Andre Boothe <andreboothe@Andres-MacBook-Pro.local>
2022-10-12 12:38:05 -0500

    added redux state management

---
+++ src/state/reducers/repositoriesReducer.ts
@@ -0,0 +1,26 @@
+import { ActionType } from '../action-types/index';
+import { Action } from '../actions';
+
+interface RepositoriesState {
+  loading: boolean;
+  error: string | null;
+  data: string[];
+}
+
+const reducer = (
+  state: RepositoriesState,
+  action: Action
+): RepositoriesState => {
+  switch (action.type) {
+    case ActionType.SEARCH_REPOSITORIES:
+      return { loading: true, error: null, data: [] };
+    case ActionType.SEARCH_REPOSITORIES_ERROR:
+      return { loading: false, error: action.payload, data: [] };
+    case ActionType.SEARCH_REPOSITORIES_SUCCESS:
+      return { loading: false, error: null, data: action.payload };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
